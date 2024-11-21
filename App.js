a2db599bf4c57e93fd58805841fcbcb4de8e7a2f
muhammadfarhan <hanyacmc123@gmail.com>
2024-11-21 15:31:37 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Teks di tengah */}
+      <Text style={styles.text}>cao ni ma</Text>
+
+      {/* Kotak kiri dan kanan */}
+      <View style={styles.boxContainer}>
+        <View style={[styles.box, styles.boxLeft]} />
+        <View style={[styles.box, styles.boxRight]} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d3d3d3', // Abu-abu terang
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'blue',
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%', // Lebar tata letak kotak
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  boxLeft: {
+    backgroundColor: 'red', // Warna bebas
+  },
+  boxRight: {
+    backgroundColor: 'green', // Warna bebas
   },
 });
+
+export default App;
